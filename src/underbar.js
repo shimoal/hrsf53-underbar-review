c339c703286dab925e94ab77cc02f99be443b306
Hack Reactor Students <accounts@hackreactor.com>
2016-12-13 16:16:37 -0800

    Add functionality to every - extend

--- src/underbar.js
+++ src/underbar.js
@@ -208,12 +208,30 @@
   // Determine whether all of the elements match a truth test.
   _.every = function(collection, iterator) {
     // TIP: Try re-using reduce() here.
+    // pass collection, iterator to reduce, set acummulator to true
+    iterator = iterator === undefined ? _.identity : iterator;
+
+    return _.reduce(collection, function(memo, item) {
+      if (!iterator(item)) {
+        return false;
+      } else {
+        return memo;
+      }
+    }, true);
   };
 
   // Determine whether any of the elements pass a truth test. If no iterator is
   // provided, provide a default one
   _.some = function(collection, iterator) {
     // TIP: There's a very clever way to re-use every() here.
+    iterator = iterator || _.identity;
+    return _.reduce(collection, function(memo, item) {
+      if (memo || iterator(item)) {
+        memo = true;
+        return memo;
+      }
+      return false;
+    }, false);
   };
 
 
@@ -236,6 +254,16 @@
   //     bla: "even more stuff"
   //   }); // obj1 now contains key1, key2, key3 and bla
   _.extend = function(obj) {
+    // iterate over arguments
+    for (var i = 1; i < arguments.length; i++) {
+      var currentArgument = arguments[i];
+      // iterate over keys in current argument
+      for (var key in currentArgument) {
+        // push each key/value pair into obj
+        obj[key] = currentArgument[key];
+      }
+    }
+    return obj;
   };
 
   // Like extend, but doesn't ever overwrite a key that already
